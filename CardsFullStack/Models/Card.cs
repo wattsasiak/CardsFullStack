4cda78f731dbe21615b7395f4bef25985d72284a
Asia Watts <AsiaWatts@quickenloans.com>
2021-11-03 11:26:39 -0400

    adding Card and Deck classes

---
+++ CardsFullStack/Models/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardsFullStack.Models
+{
+    public class Card
+    {
+
+        public int id { get; set; }
+        public string deck_id { get; set; }
+        public string image { get; set; }
+        public string cardcode {get; set;}
+
+        public string username { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
---
+++ CardsFullStack/Models/Deck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardsFullStack.Models
+{
+    public class Deck
+    {
+        public string deck_id { get; set; }
+        public bool is_current { get; set; }
+        public string username { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
